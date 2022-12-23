d8e6786636a1d3dce99059901e3b1c4084777857
nishadevi46 <nishadevi4628@gmail.com>
2022-12-23 16:45:08 +0530

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function hide(){
+   let btn=document.getElementById('btn1');
+   let para=document.getElementById('para11');
+   if(para.display.style !='none'){
+   para.style.display='none';}
+   else{
+   para.style.display='block';}
+}
