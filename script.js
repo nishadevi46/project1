add9dce7f51087c179595e6d186c39a245b89361
nishadevi46 <nishadevi4628@gmail.com>
2022-12-24 18:08:52 +0530

    commit

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function hide(){
    let btn=document.getElementById('btn1');
    let para=document.getElementById('para11');
+   
    if(para.display.style !='none'){
    para.style.display='none';}
    else{
-   para.style.display='block';}
+   para.style.display='inline-flex';}
 }
 function toggle(){
     let btn=document.getElementById('btn2');
-    let para=document.getElementByclass('container3');
+    let para=document.getElementByclass('para12');
     if(para.display.style !='none'){
     para.style.display='none';}
     else{
